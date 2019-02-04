b4ef5cdf06673f8e60d6f6bc58d8362d14922b78
Ananya Paul <ap2273@cornell.edu>
2019-02-03 19:14:35 -0500

    added scene and client code

--- Assets/ClientObject.cs
+++ Assets/ClientObject.cs
@@ -74,6 +74,12 @@ public class NetMqListener
 public class ClientObject : MonoBehaviour
 {
     private NetMqListener _netMqListener;
+    [SerializeField]
+    protected GameObject head;
+    [SerializeField]    
+    protected GameObject neck;
+
+    private LineRenderer head_neck;
 
     private void HandleMessage(string message)
     {
@@ -82,11 +88,21 @@ public class ClientObject : MonoBehaviour
         var x = float.Parse(splittedStrings[0]);
         var y = float.Parse(splittedStrings[1]);
         var z = float.Parse(splittedStrings[2]);
-        transform.position = new Vector3(x, y, z);
+        //transform.position = new Vector3(x, y, z);
+        
+        setJoints(x,y,z);
+        drawLines();
+    }
+    private void setJoints(float x, float y,float z)
+    {
+        head.transform.position = new Vector3(x+0,y+0,z+0);
+        neck.transform.position = new Vector3(x+1,y+1,z+1);
     }
     private void initializeLines()
     {
-        LineRenderer head_neck = gameObject.AddComponent<LineRenderer>();
+        //LineRenderer head_neck = gameObject.AddComponent<LineRenderer>();
+        head_neck = new LineRenderer();
+        /**
         LineRenderer neck_r_shoulder = gameObject.AddComponent<LineRenderer>();
         LineRenderer neck_l_shoulder = gameObject.AddComponent<LineRenderer>();
         LineRenderer r_shoulder_r_elbow = gameObject.AddComponent<LineRenderer>();
@@ -100,16 +116,26 @@ public class ClientObject : MonoBehaviour
         LineRenderer l_hip_l_knee= gameObject.AddComponent<LineRenderer>();
         LineRenderer r_knee_r_ankle = gameObject.AddComponent<LineRenderer>();
         LineRenderer l_knee_l_ankle= gameObject.AddComponent<LineRenderer>();
+         */
+
+    }
+    private void drawLines()
+    {
+        head_neck.SetPosition(0, head.transform.position);
+        head_neck.SetPosition(1, neck.transform.position);
+
     }
 
     private void Start()
     {
         _netMqListener = new NetMqListener(HandleMessage);
         _netMqListener.Start();
+        
     }
 
     private void Update()
     {
+        initializeLines();
         _netMqListener.Update();
     }
 
